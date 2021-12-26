eda179cdb47326089dda48d6986c35359a530746
The fat man 🐷🦄🛡️📈 <oisin.mcnamara@gmail.com>
2021-12-26 19:58:41 +0000

    started forum page

---
+++ ForumHome.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="css/forumHome.css?v=<?php echo time(); ?>">
+    <title>Forum</title>
+</head>
+<body>
+   <?php include("header.html")?>
+
+   <div class = "selection">
+        <ul> Select a topic to chat about
+        <li><a href="polymorphismEdit.php"><text>Object/classes<text></a></li>
+        <li><a href="polymorphismEdit.php">Polymorphism</a></li>
+        <li><a href="addTask.php">AddTask</a></li>
+    </ul>
+    </div>
+</body>
+</html>
