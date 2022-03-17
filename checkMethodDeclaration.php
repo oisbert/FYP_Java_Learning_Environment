c22ebeecf38cc7d0c985169eb7fd435a6dacc2c8
Oisin McNamara🐷🦄🛡️📈 <oisin.mcnamara@gmail.com>
2022-03-17 18:23:28 +0000

    comments added

--- checkMethodDeclaration.php
+++ checkMethodDeclaration.php
@@ -1,45 +1,53 @@
 <?php 
-
+/*
+    function for test 3 in the autograder checks
+    checks if the method name is declared with a lower case
+*/
 function checkMethodName($fileOpen){
     $private = "private";
     $public = "public";
-    //$publicClass = "public class";
+
+    //set count to 0 
     $count = 0;
+    //open the file and store the contents in holder
     $holder = fopen($fileOpen,"r");
+    //while its not the end of the file
     while(!feof($holder)) {
+        //get each line of the file
         $line = fgets($holder);
+
+        //if the line does contain public or private do the following..
         if(strpos($line, $public) !== false || strpos($line, $private) !== false){
+            //make an array of substrings starting with the word public
             $result = preg_split('/public/', $line);
+            //get the next word in the line after public
             $result_split = explode(' ', $result[1]);
             $answer = $result_split[1];
-            if ($answer == 'class')
+            //if the word is not class do the following
+            if ($answer != 'class')
             {
-            //echo "\"{$answer}\" correct static method found";
-            //return true;
-            
-            }
-            else{
+                //get the last word iun the line before "("
                 $first = strtok($line, '(');
 
                 $pieces = explode(' ', $first);
                 $last_word = array_pop($pieces);
-
-                if (preg_match('~^\p{Ll}~u', $last_word))
-                {
-                    //null
-                }
-                else
+                //if the word found does not start with a captital add 1 to the counter
+                if (preg_match('~^\p{Ll}~u', $last_word) == 0)
                 {
                     $count++;
                 }
+            
             } 
         }
     }
+    //if the counter is greater than 0 return false
     if ($count > 0){
         return false;
     }
+    //else return true 
     else{
         return true;
     }
+    //close the file that was opened
     fclose($fileOpen);
 }
--- checkOutput.php
+++ checkOutput.php
@@ -1,14 +1,17 @@
 <?php
 
+//Function to check the format and output tests 4 and 5 in the auto-grader
 function OutputChecker($file1,$file2, &$FormatCheck, &$outputCheck){
+    //delete the .java at the end of the files being read in 
     $file1trim = trim($file1, ".java");
     $file2trim = trim($file2, ".java");
 
-    $file2trim = trim($file2, ".java");
+    //execute the user file in the server shell and store in output 
     $output = shell_exec("javac {$file1trim}.java && java {$file1trim}");
      shell_exec("javac {$file1trim}.java > outputchecker.txt 2>&1");
      nl2br(file_get_contents( "outputchecker.txt" ));
 
+     //execute the answer file in the server shell at the same time and store in output2 
     $output2 = shell_exec("javac {$file2trim}.java && java {$file2trim}");
      shell_exec("javac {$file2} > outputchecker.txt 2>&1");
      nl2br(file_get_contents( "outputchecker.txt" ));
@@ -18,18 +21,12 @@ function OutputChecker($file1,$file2, &$FormatCheck, &$outputCheck){
 
     $outputNoSpace2 = str_replace(' ', '', $output2);
     $FormatCheck = 0;
-    if($output == $output2){
-        //echo "Output format match";
-    }
-    else{
+    if($output != $output2){
         $FormatCheck = 1;
     }
 
     $outputCheck = 0;
-    if($outputNoSpace1 == $outputNoSpace2){
-        //echo "Output is correct";
-    }
-    else{
+    if($outputNoSpace1 != $outputNoSpace2){
         $outputCheck = 1;
     }
 
